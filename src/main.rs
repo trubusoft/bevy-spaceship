a8aebcac46a6646b013302d7696dc784cb7bdb09
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2024-07-17 17:22:01 +0700

    ep1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,58 @@
 use bevy::DefaultPlugins;
-use bevy::prelude::App;
+use bevy::prelude::{App, Commands, Component, Entity, Query, Startup, Update};
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, spawn_spaceship)
+        .add_systems(Update, (
+            apply_velocity,
+            print_position
+        ))
+        .run();
+}
+
+
+#[derive(Component, Debug)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Component, Debug)]
+struct Velocity {
+    x: f32,
+    y: f32,
+}
+
+fn spawn_spaceship(
+    mut commands: Commands
+) {
+    commands.spawn((
+        Position {
+            x: 0.0,
+            y: 0.0,
+        },
+        Velocity {
+            x: 0.2,
+            y: 0.1,
+        }
+    ));
+}
+
+fn apply_velocity(
+    mut query: Query<(&Velocity, &mut Position)>
+) {
+    for (velocity, mut position) in query.iter_mut() {
+        position.x += velocity.x;
+        position.y += velocity.y;
+    }
+}
+
+fn print_position(
+    mut query: Query<(Entity, &Position, &Velocity)>
+) {
+    for (entity, position, velocity) in query.iter_mut() {
+        println!("Entity {:?} Position {:?} Velocity {:?}", entity, position, velocity);
+    }
 }
