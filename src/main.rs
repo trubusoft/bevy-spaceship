3f80127df5639f176fe3887dfe4730ea11069fc8
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2024-07-18 10:08:01 +0700

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 use bevy::DefaultPlugins;
-use bevy::prelude::App;
+use bevy::prelude::{AmbientLight, App, ClearColor, Color};
 
 use debug::DebugPlugin;
 use movement::MovementPlugin;
 use spaceship::SpaceshipPlugin;
 
+use crate::camera::CameraPlugin;
+
+mod camera;
 mod debug;
 mod movement;
 mod spaceship;
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.0, 0.15)))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 0.75,
+        })
         .add_plugins(DefaultPlugins)
+        .add_plugins(CameraPlugin)
         .add_plugins(MovementPlugin)
         .add_plugins(DebugPlugin)
         .add_plugins(SpaceshipPlugin)
