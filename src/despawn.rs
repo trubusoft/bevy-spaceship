5e8dc2d68cd6e9fc70444ea615755b78293baac1
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2024-07-19 13:38:19 +0700

    fix entity doesn't exist warning when despawning far away entities
    the former systems query for `all` entities, including `SceneBundle`, for example.
    
    The despawn_recursive() is then issued to both the parent Asteroid and the inner `SceneBundle`, hence the warning

--- src/despawn.rs
+++ src/despawn.rs
@@ -1,17 +1,23 @@
 use bevy::app::App;
 use bevy::prelude::{
-    Commands, DespawnRecursiveExt, Entity, GlobalTransform, Plugin, Query, Update, Vec3,
+    Commands, DespawnRecursiveExt, Entity, GlobalTransform, Or, Plugin, Query, Update, Vec3, With,
 };
+
+use crate::asteroid::Asteroid;
+use crate::spaceship::SpaceshipMisslie;
 
 pub struct DespawnPlugin;
 
 impl Plugin for DespawnPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, despawn_far_away_entities);
+        app.add_systems(Update, despawn_far_away_objects);
     }
 }
 
-fn despawn_far_away_entities(mut commands: Commands, query: Query<(Entity, &GlobalTransform)>) {
+fn despawn_far_away_objects(
+    mut commands: Commands,
+    query: Query<(Entity, &GlobalTransform), Or<(With<Asteroid>, With<SpaceshipMisslie>)>>,
+) {
     for (entity, global_transform) in query.iter() {
         let distance = global_transform.translation().distance(Vec3::ZERO);
         if DESPAWN_DISTANCE <= distance {
